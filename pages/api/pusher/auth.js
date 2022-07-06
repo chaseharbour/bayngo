2f5f4119bcf45c125d6b3572e983188dc0b34024
Corey Burkhart <hello@coreyburkhart.com>
2022-07-05 21:15:30 -0700

    prog: rendering tiles on [slug].js route after board create

--- pages/api/pusher/auth.js
+++ pages/api/pusher/auth.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 
 export const pusher = new Pusher({
   //CREATE ENV
-  app_id: process.env.PUSHER_APP_ID,
+  appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_KEY,
   secret: process.env.PUSHER_SECRET,
   cluster:"us3",
@@ -15,8 +15,7 @@ export default function handler(req, res) {
   const { socket_id: socketId, channel_name: channelName } = req.body;
 
   const presenceData = {
-    user_id: uuidv4(),
-    user_info: { isCreator: true },
+    user_id: uuidv4(),  
   };
 
   const auth = pusher.authorizeChannel(
@@ -24,12 +23,6 @@ export default function handler(req, res) {
     channelName,
     presenceData
   );
-
-  // const trigger = await pusher.trigger(channelName, "create-event", {
-  //   board: DEFAULT_STATE,
-  // });
-
-  // console.log(JSON.stringify(JSON.parse(response)));
 
   res.send(auth);
 }
--- pages/api/pusher/room-create.js
+++ pages/api/pusher/room-create.js
@@ -2,7 +2,7 @@ import Pusher from "pusher";
 
 export const pusher = new Pusher({
   //CREATE ENV
-  app_id: process.env.PUSHER_APP_ID,
+  appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_KEY,
   secret: process.env.PUSHER_SECRET,
   cluster: "us3",
@@ -12,7 +12,9 @@ export const pusher = new Pusher({
 export default async function createHandler(req, res) {
   const { roomID, state } = req.body;
 
-  const response = await pusher.trigger(`presence-${roomID}`, "create-event", {
+  // FIXME: have to wait for [slug].js route to subscribe before we post the event 
+  await new Promise(res => setTimeout(res, 2000)); // wait 2 seconds
+  await pusher.trigger(`presence-${roomID}`, "create-event", {
     board: state,
   }).catch(e => console.error(e));
 
