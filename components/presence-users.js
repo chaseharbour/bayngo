ff3cbb49cb34da050bafaccc56ac6e50c7b8712f
chaseharbour <harbourcp@gmail.com>
2022-07-05 22:36:18 -0700

    fix(presence-users.js): Added comment to explain function and use of the component

--- components/presence-users.js
+++ components/presence-users.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import User from "./user";
 
 const PresenceUsers = ({ users }) => {
+  //"users" prop must be an array of objects in the form of: {id: number, name: string, board: array}
+
+  //the "users" prop data should originate from Pusher event bindings in the parent component
+
   const [roomUsers, setRoomUsers] = useState([]);
 
   useEffect(() => {
